222f2009ae840849962f8c9a0edcfc6973347224
Zhu Zhe 朱哲 <466648706@qq.com>
2021-06-16 21:04:51 +0800

    yolo test

--- opencv/crop-back.py
+++ opencv/crop-back.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
 def build_filters():
     filters = []
@@ -36,41 +35,23 @@ _cy = 718 + _cr
 
 res = process(new_img, filters)
 
-#cv2.imshow('res', res)
-#cv2.waitKey(0)
+cv2.imshow('res', res)
+cv2.waitKey(0)
 
 gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
 
-#cv2.imshow('gray', gray)
-#cv2.waitKey(0)
+cv2.imshow('gray', gray)
+cv2.waitKey(0)
 
 mask = np.zeros((_w0,_h0),dtype=np.uint8)
 circle_img = cv2.circle(mask,(_cx,_cy),_cr, 1 ,-1)
 masked_data = cv2.bitwise_and(gray, gray, mask=circle_img)
 cv2.circle(masked_data,(_cx,_cy), int(_cr * 0.8), 0,-1)
 
-#cv2.imshow('masked_data', masked_data)
-#cv2.waitKey(0)
+cv2.imshow('masked_data', masked_data)
+cv2.waitKey(0)
 
 ret, thresh = cv2.threshold(masked_data, 172, 255, cv2.THRESH_BINARY)
 
-#cv2.imshow('thresh', thresh)
-#cv2.waitKey(0)
-
-### 
-
-_nz = np.nonzero(thresh)
-
-_xls = _nz[1]
-_yls = _nz[0]
-_len = len(_xls)
-_res = []
-
-for _k in range(_len):
-    _dot = (_xls[_k]-_cx, _yls[_k]-_cy)
-    if (_dot[0] != 0):
-        _res.append(_dot[1]/_dot[0])
-
-
-plt.hist(_res, 180, [-3, 3])
-plt.show()
+cv2.imshow('thresh', thresh)
+cv2.waitKey(0)
--- opencv/hough.py
+++ opencv/hough.py
@@ -1,34 +1,16 @@
 import numpy as np
-import cv2
-
-image = cv2.imread("./imgs/test2.jpeg",0)
-output = cv2.imread("./imgs/test2.jpeg",1)
-cv2.imshow("Original image", image)
-cv2.waitKey()
-
-blurred = cv2.GaussianBlur(image,(11,11),0)
-
-cv2.imshow("Blurred image", blurred)
-cv2.waitKey()
-
-# Finds circles in a grayscale image using the Hough transform
-circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 100,
-                             param1=100,param2=90,minRadius=0,maxRadius=200)
-
-# cv2.HoughCircles function has a lot of parameters, so you can find more about it in documentation
-# or you can use cv2.HoughCircles? in jupyter nootebook to get that 
-
-# Check to see if there is any detection
-if circles is not None:
-    # If there are some detections, convert radius and x,y(center) coordinates to integer
-    circles = np.round(circles[0, :]).astype("int")
-
-    for (x, y, r) in circles:
-        # Draw the circle in the output image
-        cv2.circle(output, (x, y), r, (0,255,0), 3)
-        # Draw a rectangle(center) in the output image
-        cv2.rectangle(output, (x - 2, y - 2), (x + 2, y + 2), (0,255,0), -1)
-
-cv2.imshow("Detections",output)
-cv2.imwrite("CirclesDetection.jpg",output)
-cv2.waitKey()
+import cv2 as cv
+img = cv.imread('./imgs/test3.jpeg',0)
+img = cv.medianBlur(img,5)
+cimg = cv.cvtColor(img,cv.COLOR_GRAY2BGR)
+circles = cv.HoughCircles(img,cv.HOUGH_GRADIENT,1,20,
+                            param1=50,param2=30,minRadius=0,maxRadius=0)
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+cv.imshow('detected circles',cimg)
+cv.waitKey(0)
+cv.destroyAllWindows()
