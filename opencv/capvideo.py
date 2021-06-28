dbf5563e7759d3eba9e174b457d36a73c23e7652
Zhu Zhe 朱哲 <466648706@qq.com>
2021-06-28 10:30:42 +0800

    图像拼接

---
+++ opencv/capvideo.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture(0)
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'mp4v')
+
+output = "./out/"
+
+#out = cv.VideoWriter('./out/output.mp4', fourcc, 2.0, (640,  480))
+count = 0
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    #frame = cv.flip(frame, 0)
+    # write the flipped frame
+    #out.write(frame)
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(1) == ord('r'):
+        _name = output + str(count)
+        cv.imwrite(_name + ".jpg", frame)
+        print(_name, "saved")
+        count += 1
+
+    if cv.waitKey(1) == ord('q'):
+        break
+# Release everything if job is finished
+cap.release()
+#out.release()
+cv.destroyAllWindows()
---
+++ opencv/stitching.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+'''
+Stitching sample
+================
+Show how to use Stitcher API from python in a simple way to stitch panoramas
+or scans.
+'''
+
+# Python 2/3 compatibility
+from __future__ import print_function
+
+import numpy as np
+import cv2 as cv
+
+import argparse
+import sys
+
+from os import walk
+
+modes = (cv.Stitcher_PANORAMA, cv.Stitcher_SCANS)
+
+parser = argparse.ArgumentParser(prog='stitching.py', description='Stitching sample.')
+parser.add_argument('--mode',
+    type = int, choices = modes, default = cv.Stitcher_PANORAMA,
+    #type = int, choices = modes, default = cv.Stitcher_SCANS,
+    help = 'Determines configuration of stitcher. The default is `PANORAMA` (%d), '
+         'mode suitable for creating photo panoramas. Option `SCANS` (%d) is suitable '
+         'for stitching materials under affine transformation, such as scans.' % modes)
+parser.add_argument('--output', default = 'result.jpg',
+    help = 'Resulting image. The default is `result.jpg`.')
+#parser.add_argument('img', nargs='+', help = 'input images')
+
+__doc__ += '\n' + parser.format_help()
+__path = "./stitching/"
+
+def main():
+    args = parser.parse_args()
+
+    # read input images
+    imgs = []
+
+    for (dirpath, dirnames, filenames) in walk(__path):
+        for filename in filenames:
+            print(filename)
+            img = cv.imread(__path + filename)
+            imgs.append(img)
+
+    """
+    for img_name in args.img:
+        img = cv.imread(__path + img_name)
+        if img is None:
+            print("can't read image " + img_name)
+            sys.exit(-1)
+        imgs.append(img)
+    """
+
+    stitcher = cv.Stitcher.create(args.mode)
+    status, pano = stitcher.stitch(imgs)
+
+    if status != cv.Stitcher_OK:
+        print("Can't stitch images, error code = %d" % status)
+        sys.exit(-1)
+
+    cv.imwrite(args.output, pano)
+    print("stitching completed successfully. %s saved!" % args.output)
+
+    print('Done')
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    main()
+    cv.destroyAllWindows()
+
+# python stiching.py 1.jpeg 2.jpeg 3.jpeg 4.jpeg 5.jpeg 6.jpeg 7.jpeg 8.jpeg
